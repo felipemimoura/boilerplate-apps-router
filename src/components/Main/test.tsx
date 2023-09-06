2a2b9902d4735d1c7d6f12aa34cac1ac7537953a
Felipe Moura <felipemimoura@gmail.com>
2023-09-05 21:03:47 -0300

    aula_21 configurando snapshots com jest-styled-components

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -14,4 +14,10 @@ describe('<Main />', () => {
     //Gerar snapshot
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
